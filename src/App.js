22ce9b13b0af3ac5839648dc29b6857405d97e35
Twisha <twishask@gmail.com>
2019-04-10 17:23:30 +0530

    Included search bar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,25 @@ class App extends Component {
     super()
     this.state = {
       data: [],
+      input: '',
+      trending: [],
     }
+  }
+  
+  componentDidMount() {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=JYslkGeco8QJ6gAUF3hRIpQSltMs0hsF&limit=25&rating=G')
+      .then((result) => {
+        return result.json();
+      }).then((jsonResult) => {
+        var trending = jsonResult.data;
+        var array = [];
+        for (var i = 0; i < trending.length; i++) {
+          array[i] = trending[i].images.fixed_height.url;
+        }
+        this.setState ({
+          trending: array,
+          })
+      }) 
   }
   
   random = e => {
@@ -27,9 +45,36 @@ class App extends Component {
     })
   }
   
-  callGiphy = e => {
+  handleInput = e =>{
+    this.setState ({
+      input: e.target.value
+    })
+  }
+  
+  search = e => {
     e.preventDefault()
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=JYslkGeco8QJ6gAUF3hRIpQSltMs0hsF&limit=25&rating=G')
+    this.setState ({
+      data: [],
+    })
+    var params = this.state.input
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=JYslkGeco8QJ6gAUF3hRIpQSltMs0hsF&q='+params+'&limit=25&offset=0&rating=G&lang=en')
+      .then((result) => {
+        return result.json();
+      }).then((jsonResult) => {
+        var data = jsonResult.data;
+        var array = [];
+        for (var i = 0; i < data.length; i++) {
+          array[i] = data[i].images.fixed_height.url;
+        }
+        this.setState ({
+          data: array,
+          })
+      }) 
+  }
+    
+  trending = e => {
+    e.preventDefault()
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=JYslkGeco8QJ6gAUF3hRIpQSltMs0hsF&limit=50&rating=G')
       .then((result) => {
         return result.json();
       }).then((jsonResult) => {
@@ -45,21 +90,31 @@ class App extends Component {
   }
   
   render() {
-    let gifs = this.state.data
     return (
       <div className="App">
-      <input type="button" value="GIFs from GIPHY" onClick={this.callGiphy} />
+      <form>
+      <input type="text" placeholder="Search for GIFs" onChange={this.handleInput} />
+      <button onClick={this.search}>Search</button>
       <br></br>
       <input type="button" value="Random from GIPHY" onClick={this.random} />
       <br></br>
       <input type="button" value="Reverse the order" onClick={this.reverse} />
       <br></br>
-        {gifs.map(gif => {
+      </form>
+      {this.state.data.map(gif => {
           return(
             <img src= {gif} />
           )})
-        }
-        </div>
+      }
+      <h2>Trending GIFs from GIPHY</h2>
+      {this.state.trending.map(gif => {
+        return(
+          <img src= {gif} />
+        )})
+      }
+      <br></br>
+      <input type="button" value="Load more trending GIFs" onClick={this.trending} />
+      </div>
     );
   }
 }
